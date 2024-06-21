a66adf52bb50d1925003546d30747db85eeb045a
Lucas <lucas.txsan@gmail.com>
2024-06-21 14:31:53 -0300

    Created Typography

---
+++ src/assets/styles/typography.styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-h1);
+    line-height: var(--spacing-90);
+`; 
+
+export const H2 = styled.h2`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-h2);
+    line-height: var(--spacing-90);
+`; 
+
+export const H3 = styled.h3`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-h3);
+    line-height: var(--spacing-100);
+`;
+
+export const Text = styled.p`
+    font-family: var(--font-family-helvetica);
+    font-size: var(--font-size-h6);
+    line-height: var(--spacing-200);
+`; 
+
