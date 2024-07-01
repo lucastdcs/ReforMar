5b598d61360f8a593d94baa011f3ad6c48328f86
Lucas <lucas.txsan@gmail.com>
2024-07-01 14:20:31 -0300

    Added some Mobile Design Configs

--- src/assets/styles/typography.styled.js
+++ src/assets/styles/typography.styled.js
@@ -14,6 +14,13 @@ export const H1 = styled.h1`
     }
 `; 
 
+export const MH1 = styled.h1`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-mh1);
+    line-height: var(--spacing-100);
+
+`; 
+
 export const H2 = styled.h2`
     font-family: var(--font-family-grifter);
     font-size: var(--font-size-h2);
@@ -26,6 +33,13 @@ export const H2 = styled.h2`
     @media (max-width: 480px) {
         font-size: 2rem;
     }
+`; 
+
+export const MH2 = styled.h2`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-mh2);
+    line-height: var(--spacing-100);
+
 `; 
 
 export const H3 = styled.h3`
@@ -42,6 +56,13 @@ export const H3 = styled.h3`
     }
 `;
 
+export const MH3 = styled.h3`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-mh3);
+    line-height: var(--spacing-100);
+
+`;
+
 export const Text = styled.p`
     font-family: var(--font-family-helvetica);
     font-size: var(--font-size-h6);
@@ -54,6 +75,13 @@ export const Text = styled.p`
     @media (max-width: 480px) {
         font-size: 1.2rem;
     }
+`;
+
+export const MText = styled.p`
+    font-family: var(--font-family-helvetica);
+    font-size: var(--font-size-mp );
+    line-height: var(--spacing-200);
+
 `;
   
 export const Label = styled.a`
@@ -76,6 +104,22 @@ export const Label = styled.a`
     @media (max-width: 480px) {
         font-size: ${props => (props.primary ? '1.6rem' : '1.2rem')};
     }
+
+`; 
+
+export const MLabel = styled.a`
+    font-family: var(--font-family-grifter);
+    font-size: var(--font-size-mlabel1);
+    line-height: var(--spacing-160);
+    text-transform: uppercase;
+    white-space: nowrap;
+
+    letter-spacing: 0.4rem;
+
+    cursor: pointer;
+
+    font-size: ${props => (props.primary ? 'var(--font-size-mlabel2)' : 'var(--font-size-mlabel1)')};
+
 
 `; 
 
