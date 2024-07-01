d45e53b0a662ed43301da26b18fec7501bca1ffc
Lucas <lucas.txsan@gmail.com>
2024-06-30 23:38:26 -0300

    Fixed some css issues

--- src/Components/Header/Header.styled.js
+++ src/Components/Header/Header.styled.js
@@ -5,108 +5,102 @@ import backArrow from "../../assets/icons/navigate_before.svg"
 
 const StyledHeader = styled.header`
     max-width: 192rem;
-    width: 100vw;
-    height: auto;
-    padding: 4rem;
+  width: 100vw;
+  height: auto;
+  padding: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15vw;
+
+  .wrapper {
+    width: 20%;
+    max-width: 50rem;
     display: flex;
-    align-items: center;
     justify-content: center;
-    gap: 15vw;
-
-    .wrapper {
-        width: 20%;
-        max-width: 50rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 5rem;
-    }
-
-    .wrapper2 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 10rem;
-    }
-
-    span {
-        display: flex;
-        gap: 3rem;
-        height: 1.5rem;
-    }
-
-    span a{
-        margin: 0;
-        height: min-content;
-    }
-
-    #logo {
-        max-height: 6rem;
-        max-width: 100%; /* Ajusta o logo ao contêiner pai */
-        width: 15vw; /* Ajusta o logo de acordo com a largura da viewport */
+    align-items: center;
+    gap: 5rem;
   }
 
-  #logo_pequeno{
+  .wrapper2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10rem;
+  }
+
+  span {
+    display: flex;
+    gap: 3rem;
+    height: 1.5rem;
+  }
+
+  span a {
+    margin: 0;
+    height: min-content;
+  }
+
+  #logo {
+    max-height: 6rem;
+    max-width: 100%; /* Ajusta o logo ao contêiner pai */
+    width: 15vw; /* Ajusta o logo de acordo com a largura da viewport */
+  }
+
+  #logo_pequeno {
     display: none;
   }
 
+  nav {
+    max-width: 56rem;
+    display: flex;
+    gap: 2vw;
+  }
 
-    nav {
-        max-width: 56rem;
-        display: flex;
-        /* justify-content: space-between; */
-        gap: 2vw;
+  nav a {
+    text-decoration: none;
+  }
+
+  #nav_menu {
+    display: none;
+  }
+
+  @media (max-width: 1800px) {
+    .wrapper {
+      gap: 5rem;
     }
-
-    nav a {
-        text-decoration: none;
-    }
-
-
-    #nav_menu{
-            display: none;
-        }
-
-    @media (max-width: 1800px) {
-        .wrapper {
-        gap: 5rem;
-    }
-        .wrapper2 {
-        gap: 5rem;
+    .wrapper2 {
+      gap: 5rem;
     }
 
     #logo {
-        max-height: 6rem;
-        max-width: 100%; /* Ajusta o logo ao contêiner pai */
-        width: 13vw; /* Ajusta o logo de acordo com a largura da viewport */
+      max-height: 6rem;
+      max-width: 100%; /* Ajusta o logo ao contêiner pai */
+      width: 13vw; /* Ajusta o logo de acordo com a largura da viewport */
     }
   }
-    @media (max-width: 1300px) {
-        .wrapper2 nav a {
-            font-size: var(--font-size-label10);
-        }
-
-        #logo_pequeno{
-            display: block;
-        }
-
-        #logo{
-            display: none;
-        }
-  }
-    @media (max-width: 1060px) {
-
-        #btn_contact {
-            display: none;
-        }
-        #nav_menu{
-            display: block;
-        }
-
+  @media (max-width: 1300px) {
+    .wrapper2 nav a {
+      font-size: var(--font-size-label10);
     }
-  
 
+    #logo_pequeno {
+      display: block;
+    }
+
+    #logo {
+      display: none;
+    }
+  }
+  @media (max-width: 1060px) {
+    #btn_contact {
+      display: none;
+    }
+    #nav_menu {
+      display: block;
+    }
+  }
 `;
+
 
 export const StyledNav = styled.nav`
     max-width: 56rem;
@@ -133,6 +127,31 @@ export const StyledNav = styled.nav`
         position:relative;
         z-index: 1;
 
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 4rem;
+            height: 100vh;
+            background-color: rgba(0, 0, 0, 0.8);
+            position: fixed;
+            top: 0;
+            right: -100vw;
+            width: 100vw;
+            /* transform: translateX(100%); */
+            transition: transform 0.3s ease;
+        }
+
+        &.active ul {
+            /* transform: translateX(0); */
+            right: 0;
+            flex-direction: column;
+        }
+
         &::before{
             display: none;
             content: "";
@@ -151,10 +170,6 @@ export const StyledNav = styled.nav`
             opacity: 1;
         }
 
-        &.active ul {
-            transform: translateX(0);
-            flex-direction: column;
-        }
 
         .back-arrow {
             position: fixed;
@@ -173,25 +188,6 @@ export const StyledNav = styled.nav`
         &.active .back-arrow {
             opacity: 1;
         }
-        ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            gap: 4rem;
-            height: 100vh;
-            background-color: rgba(0, 0, 0, 0.8);
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            transform: translateX(100%);
-            transition: transform 0.3s ease;
-        }
-
 
 }
 `;
