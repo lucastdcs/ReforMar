b3f153cb91242e66ac5bc1df2c359a8d7891aa4b
Lucas <lucas.txsan@gmail.com>
2024-07-04 10:31:43 -0300

    Updated Carousel Component

--- src/Components/Carrousel/Carousel.styled.js
+++ src/Components/Carrousel/Carousel.styled.js
@@ -3,19 +3,12 @@ import styled from "styled-components";
 export const CarouselContainer = styled.div`
   display: flex;
   overflow-x: scroll;
-  scroll-snap-type: x mandatory;
-  -webkit-overflow-scrolling: touch; /* Suaviza o scroll no iOS */
-
-  &::-webkit-scrollbar {
-    display: none; /* Esconde a barra de rolagem no WebKit */
-  }
+  gap: 2rem;
+  /* scroll-snap-type: x mandatory; */
 
   > * {
     scroll-snap-align: start;
     flex: 0 0 auto; /* Impede que os itens flex encolham ou cresçam */
   }
 
-  /* Para evitar que os itens ultrapassem as bordas */
-  -ms-overflow-style: none; /* Para o Internet Explorer e Edge */
-  scrollbar-width: none; /* Para o Firefox */
 `;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Importações corretas do react-router-dom
 import './index.css';
 
