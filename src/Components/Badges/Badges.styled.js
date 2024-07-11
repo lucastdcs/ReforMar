46dd0a796a9573bfb3bdd85a8278f0028cd0d8b5
Lucas <lucas.txsan@gmail.com>
2024-07-11 14:25:30 -0300

    Updated badges component

--- src/Components/Badges/Badges.styled.js
+++ src/Components/Badges/Badges.styled.js
@@ -8,13 +8,25 @@ const StyledContainer = styled.div`
     justify-content: center;
     gap: 1.6rem;
     padding: 6rem 0;
-    margin: 0 1rem;
-    overflow-x: hidden;
-    
+    /* margin: 0 1rem; */
+    overflow-x: auto; /* Permite a rolagem horizontal */
+
+    /* Esconde a scrollbar em WebKit (Chrome, Safari) */
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    /* Esconde a scrollbar no Firefox */
+    scrollbar-width: none;
+
+    /* Esconde a scrollbar no IE e Edge (antigo) */
+    -ms-overflow-style: none;
+
     @media (max-width: 515px) {
-        
+        position: relative;
+        left: 1rem;
         justify-content: start;
-        overflow-x: scroll;
+        overflow-x: auto;
     }
 `;
 
